cac8d8fa3fe2b1b2a2ad965d113172234625569b
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-04 20:19:21 +0000

    second codingstyse commit w if statement

--- coding-style.js
+++ coding-style.js
@@ -4,3 +4,10 @@ var  age = 31; // semicolon at end of statement
 
 var name = "Ziad";
 
+
+var num = 10;
+
+if (num > 0) { //always have curly brackets even if just one statement
+  console.log('num is greater than zero!!!');
+  console.log('good job!!!');
+}
