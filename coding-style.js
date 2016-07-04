9f8fdcb6058ce3dda23fc25beb709285a6b72352
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-04 20:44:53 +0000

    added functions to js file

--- coding-style.js
+++ coding-style.js
@@ -11,3 +11,25 @@ if (num > 0) { //always have curly brackets even if just one statement
   console.log('num is greater than zero!!!');
   console.log('good job!!!');
 }
+
+// Functions
+
+
+/*
+Multiline comment
+These 3 ways of writing functions are not the exact same. Use the first one.
+*/
+
+function doSomething(sentence) {
+  // ...code goes here
+  // ziad recommends this one for now
+}
+
+var doSomething = function(sentence) {
+  
+}
+
+var doSomething = function doSomething(sentence){
+  
+}
+
